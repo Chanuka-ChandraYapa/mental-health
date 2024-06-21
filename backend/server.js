e9aac363999253f7d0feb6cc572604d4b404f0fb
Chanuka Lakshan Chandra Yapa <118423471+Chanuka-ChandraYapa@users.noreply.github.com>
2024-06-21 10:27:50 +0530

    fixed user management code

--- backend/server.js
+++ backend/server.js
@@ -28,4 +28,6 @@ app.use("/api/users", require("./routes/users"));
 // app.use("/api/moods", require("./routes/moods"));
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
