8ae8543c41e82dce7f54f7900267a25d3d177d90
Chanuka Lakshan Chandra Yapa <118423471+Chanuka-ChandraYapa@users.noreply.github.com>
2024-08-11 00:30:42 +0530

    fixed a syntax issue in user management

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -67,7 +67,7 @@ exports.loginUser = async (req, res) => {
     );
     const response = await fetch(
       // "https://mental-health-chatbot-dlhq.onrender.com/chatbot",
-      "https://mental-health-chatbot-production.up.railway.app/chatbot"
+      "https://mental-health-chatbot-production.up.railway.app/chatbot",
       // "http://127.0.0.1:5000/chatbot",
       {
         method: "POST",
